1c2015d6a2e7a9b1f47914f9cb9407f4dff46886
MaciejSL3 <81634287+MaciejSL3@users.noreply.github.com>
2021-03-30 17:26:49 +0200

    Add files via upload

--- Matrix_test.m
+++ Matrix_test.m
@@ -1,9 +1,10 @@
 clear all
 clc
 
-N = 6
-A = 10*eye(N)
+P = 'Podaj wielkość swojej macierzy: '
+N = input(P)
+X = 10*eye(N)
 B = diag(-5 * ones(N-1,1), 1)
 C = diag(5 * ones(N-1,1), -1)
-ABC = A + B + C
-CBA = fliplr(ABC)
+ABC = X + B + C
+A = fliplr(ABC)
